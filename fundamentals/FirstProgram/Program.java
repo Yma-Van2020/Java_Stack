10883ae90ca83a1bd5bb237c37f1d9d08915f9d8
Yucong Ma <candicema2018@gmail.com>
2021-09-24 22:12:15 -0700

    finish

---
+++ fundamentals/FirstProgram/Program.java
@@ -0,0 +1,7 @@
+public class Program {
+    public static void main(String[] args){
+        System.out.println("My name is Katelyn");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Canada");
+    }
+}
