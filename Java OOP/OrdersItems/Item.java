833377209da6f191f562c85e33723ac00857e279
Yucong Ma <candicema2018@gmail.com>
2021-09-27 18:03:13 -0700

    finish

--- Java OOP/OrdersItems/Item.java
+++ Java OOP/OrdersItems/Item.java
@@ -1,4 +1,30 @@
 public class Item {
-    public String name;
-    public double price;
+    private String name;
+    private double price;
+
+    //constructor
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    //getter for name
+    public String getName(){
+        return name;
+    }
+
+    //setter for price
+    public void setName(String nameToSet){
+        name = nameToSet;
+    }
+
+    //getter for price
+    public double getPrice(){
+        return price;
+    }
+
+    //setter for price
+    public void setPrice(double priceToSet){
+        price = priceToSet;
+    }
 }
--- Java OOP/OrdersItems/Order.java
+++ Java OOP/OrdersItems/Order.java
@@ -1,8 +1,77 @@
 import java.util.ArrayList;
 
 public class Order {
-    public String name;
-    public double total;
-    public boolean ready;
-    public ArrayList<Item> items;
+    private String name;
+    private boolean ready;
+    private ArrayList<Item> items;
+
+    //constructor
+    public Order(){
+        name = "Guest";
+        items = new ArrayList<Item>();
+    }
+
+    //overload constructor
+    public Order(String name){
+        this.name = name;
+        items = new ArrayList<Item>();
+    }
+
+    //getter for name
+    public String getName(){
+        return name;
+    }
+
+    //setter for name
+    public void setName(String name){
+        this.name = name;
+    }
+
+    //getter for ready
+    public Boolean getReady(){
+        return ready;
+    }
+
+    //setter for ready
+    public void setReady(Boolean ready){
+        this.ready = ready;
+    }
+
+    //getter for items
+    public ArrayList<Item> getItem(){
+        return items;
+    }
+
+    //setter for items
+    public void setItem(ArrayList<Item> items){
+        this.items = items;
+    }
+
+    public void addItem(Item item){
+         items.add(item);
+    }
+
+    public String getStatusMessage(){
+        if(ready){
+            return "Your order is ready";
+        } else {
+            return "Thank you for waitin. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal(){
+        double total = 0;
+        for(Item item:items){
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public void display(){
+            System.out.println("Customer name: " + name);
+            for(Item item:items){
+                System.out.println(item.getName() + " - $" + item.getPrice());
+             }
+            System.out.println("Total: $" + getOrderTotal();
+    }
 }
