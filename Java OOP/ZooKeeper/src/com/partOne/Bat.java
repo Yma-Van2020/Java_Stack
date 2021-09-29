93e00771035ba304efd1cfe8efffddaba268d8f2
Yucong Ma <candicema2018@gmail.com>
2021-09-28 18:23:48 -0700

    finish

---
+++ Java OOP/ZooKeeper/src/com/partOne/Bat.java
@@ -0,0 +1,19 @@
+package com.partOne;
+
+public class Bat extends Mammal {
+	public Bat() {
+		energyLevel = 300;
+	}
+	public void fly() {
+		System.out.println("oof oof"!");
+		this.energyLevel -= 50;
+	}
+	public void eatHumans() {
+		System.out.println("yummmmmm");
+		this.energyLevel += 25;
+	}
+	public void attackTown() {
+		System.out.println("hoooooo");
+		this.energyLevel -= 100;
+	}
+}
