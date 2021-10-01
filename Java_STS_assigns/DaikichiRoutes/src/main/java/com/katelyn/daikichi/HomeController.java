716f0a903447afd1734a66c5f136c6738004c376
Yucong Ma <candicema2018@gmail.com>
2021-09-30 19:53:40 -0700

    finish

--- Java_STS_assigns/DaikichiRoutes/src/main/java/com/katelyn/daikichi/HomeController.java
+++ Java_STS_assigns/DaikichiRoutes/src/main/java/com/katelyn/daikichi/HomeController.java
@@ -1,25 +1,25 @@
 package com.katelyn.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/daikichi")
 public class HomeController {
-	@RequestMapping("")
-	public String greeting() {
-		return "Welcome!";
-
+	@RequestMapping("/travel/{destination}")
+	public String travel(@PathVariable("destination") String destination) {
+		return "Congratulations! You will soon travel to " + destination;
 	}
 	
-	@RequestMapping("/today")
-		public String today() {
-			return "Today you will find luck in all your endeavors!";
+	@RequestMapping("/lotto/{number}")
+		public String today(@PathVariable("number") Integer number) {
+			if(number % 2 == 0) {
+				return  "You will take a grand journey in the near future, but be weary of tempting offers";
+			} 
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 		}
 	
-	@RequestMapping("/tomorrow")
-	   public String tmr() {
-		return "Torrow, and opportinity will arise, so be sire to be open";
-	}
 
 }
+	
