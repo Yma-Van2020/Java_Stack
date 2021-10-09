2daad085388c2b98167c931c41af95c29cb263db
Yucong Ma <candicema2018@gmail.com>
2021-10-08 20:06:22 -0700

    finish auth

---
+++ Full Spring/authentication/src/test/java/com/katelyn/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katelyn.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
