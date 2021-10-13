0e50a6ee8655bbde87ddb54180cefd5af0b90343
Katelyn <74885386+Yma-Van2020@users.noreply.github.com>
2021-10-13 09:37:23 -0700

    Update BookRepository.java

--- Full Spring/authentication/src/main/java/com/katelyn/authentication/repos/BookRepository.java
+++ Full Spring/authentication/src/main/java/com/katelyn/authentication/repos/BookRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import com.katelyn.authentication.models.Book;
 
+@Repository
 public interface BookRepository extends CrudRepository<Book, Long>{
 	List<Book> findAll();
 
